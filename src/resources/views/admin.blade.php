fc3b516f72160ee561101e1bb811928d5ef73441
shiori <a.shi_den16.e@icloud.com>
2026-02-07 19:46:25 +0900

    更新

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -85,6 +85,34 @@
                                 <form class="export-form" action="/export" method="post" >
                                     <div class="export-form__button">
                                         <button class="export-form__button-submit" type="submit" action="/export">エクスポート</button>
+
+<script>
+    @csrf
+    function handleDownload() {
+        var bom = new Uint8Array([0xEF, 0xBB, 0xBF]);//文字コードをBOM付きUTF-8に指定
+        var table = document.getElementById('table1');//id=table1という要素を取得
+        var data_csv="";//ここに文字データとして値を格納していく
+
+        for(var i = 0;  i < table.rows.length; i++){
+          for(var j = 0; j < table.rows[i].cells.length; j++){
+            data_csv += table.rows[i].cells[j].innerText;//HTML中の表のセル値をdata_csvに格納
+            if(j == table.rows[i].cells.length-1) data_csv += "\n";//行終わりに改行コードを追加
+            else data_csv += ",";//セル値の区切り文字として,を追加
+          }
+        }
+
+        var blob = new Blob([ bom, data_csv], { "type" : "text/csv" });//data_csvのデータをcsvとしてダウンロードする関数
+        if (window.navigator.msSaveBlob) { //IEの場合の処理
+            window.navigator.msSaveBlob(blob, "test.csv"); 
+            //window.navigator.msSaveOrOpenBlob(blob, "test.csv");// msSaveOrOpenBlobの場合はファイルを保存せずに開ける
+        } else {
+            document.getElementById("download").href = window.URL.createObjectURL(blob);
+        }
+        delete data_csv;//data_csvオブジェクトはもういらないので消去してメモリを開放
+    }
+    //ここまでCSV出力＆ダウンロード
+</script>
+
                                     </div>
                                 </form>
                             </div>
